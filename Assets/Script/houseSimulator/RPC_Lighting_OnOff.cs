635f27135ba72eaae04d5021f463ca9513192976
Tucker <ohoka.taka@icloud.com>
2025-09-02 10:18:12 +0900

    照明のオンオフの同期,RPC通信

--- Assets/Script/houseSimulator/RPC_Lighting_OnOff.cs
+++ Assets/Script/houseSimulator/RPC_Lighting_OnOff.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class RPC_Lighting_OnOff : MonoBehaviour
@@ -7,12 +8,28 @@ public class RPC_Lighting_OnOff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //照明のオンオフ処理、スペース
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Debug.Log("照明のオンオフ処理");
+            PhotonView photonView = PhotonView.Get(this);
+            photonView.RPC("OnOffLight", RpcTarget.All);
+
+        }
+    }
+
+    [PunRPC]
+    public void OnOffLight()
+    {
+        Light light = GetComponent<Light>();
+        light.enabled = !light.enabled;
     }
 }
